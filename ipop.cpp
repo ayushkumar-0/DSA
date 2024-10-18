c4f16f6f8c457c3d29f7f0627179e45ddbf6350b
ayushkumar-0 <ayushkumarr0721@gmail.com>
2024-10-18 14:58:33 +0530

    c++ basic input output

---
+++ ipop.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+    int y;
+    cin >>x >>y;
+    cout<< "value of x: "<<x << endl;
+    cout<< "and the value of y: "<<y;
+    return 0;
+}
